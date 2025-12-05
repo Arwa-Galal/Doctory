50ddaff9dc72e0174fe48524ddc79b682ff98257
Arwa Galal <125925915+Arwa-Galal@users.noreply.github.com>
2025-12-05 13:02:45 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ import os
 import onnxruntime as ort
 
 # --- 1. CONFIGURATION ---
-API_KEY = "AIzaSyBxYCfAwsyhbhiA8EQd6dcn-RdsZQ9xtZ8"
+API_KEY = "AIzaSyAteE4f2FfYwFwZjw17QiHVlLVsrltQQBQ"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key={API_KEY}"
 
 MEDICAL_PROMPT = """
