5e2c725975a140ce0208c92d263a63dc386de3bd
Arwa Galal <125925915+Arwa-Galal@users.noreply.github.com>
2025-12-07 14:40:42 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,10 +19,13 @@ st.write("") # Spacer
 c1, c2, c3 = st.columns([1, 1, 1])
 
 with c1:
-    st.markdown('<div class="css-card">', unsafe_allow_html=True)
-    st.image("https://cdn-icons-png.flaticon.com/512/4712/4712035.png", width=60)
-    st.subheader("AI Doctor")
-    st.caption("Chat with our smart assistant.")
+st.markdown("""
+    <div class="css-card">
+        <img src="https://cdn-icons-png.flaticon.com/512/4712/4712035.png">
+        <h3>AI Doctor</h3>
+        <p>Chat with our smart assistant.</p>
+    </div>
+    """, unsafe_allow_html=True)
     if st.button("Start Chat"):
         # تأكدي أن اسم الملف في فولدر pages هو 1_💬_Chat_With_Doctory.py
         st.switch_page("pages/1_AI_Chatbot.py")
