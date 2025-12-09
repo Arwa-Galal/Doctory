0afa99a7732c64578082aff03291db01969276b7
Arwa Galal <125925915+Arwa-Galal@users.noreply.github.com>
2025-12-09 17:08:29 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import os
 from streamlit_option_menu import option_menu 
 
 # --- CONFIGURATION ---
-API_KEY = "AIzaSyC1csmV0jQfJlCHMkqS_VeE5b0PINdDfG8"
+API_KEY = "AIzaSyC5AKcd2co2yweGFA9sIx_Og9lyw5TD34A"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key={API_KEY}"
 
 MEDICAL_PROMPT = """
